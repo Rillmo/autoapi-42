d906fed6b98dcec26e3cd41f792d7d474ea23373
김준호 <junkim2@student.42seoul.kr>
2024-10-17 17:27:35 +0900

    feat: added appInfo receiving func from user

--- app.js
+++ app.js
@@ -22,20 +22,39 @@ const getIdAndPw = async () => {
 		output: process.stdout,
 		terminal: true
 	});
-	// get id
 	const id = await rl.question('>> Please insert your intra ID : ');
 	const pw = await rl.question('>> Please insert your intra PW : ');
 	rl.close();
 	return [id,pw];
 }
 
+// get app path & url from user
+const getAppInfo = async () => {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		terminal: true
+	});
+	const appPath = await rl.question('>> Please insert your APP PATH(ex, https://profile.intra.42.fr/oauth/applications/4242) : ');
+	const appUrl = await rl.question('>> Please insert your APP URL(ex. https://123.123.123.123:4242) : ');
+	rl.close();
+	return [appPath,appUrl];
+}
+
 // main function
 const run = async () => {
 	try {
 		printWelcome();
-		const config = await getIdAndPw();
-		console.log(config);
-		config.forEach((v) => {	// check empty values
+		
+		let config = {};
+		const idpw = await getIdAndPw();
+		config.intraId = idpw[0];
+		config.intraPw = idpw[1];
+		const appInfo = await getAppInfo();
+		config.appPath = appInfo[0];
+		config.appUrl = appInfo[1];
+
+		Object.values(config).forEach((v) => {	// check empty values
 			if (!v) throw new Error('emtpy value detected');
 		});
 		fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
